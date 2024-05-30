33effe4ed761cb09bfffcd9f16710af8679d4f89
Felipe Adeildo <felipe.adeildo0@gmail.com>
2024-05-30 20:55:30 -0300

    feat(backend): Add CORS middleware allowing frontend's requests

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from app.prisma import shutdown, startup
 from app.routers import account, download, log, platform, setting
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(download.router, prefix="/download", tags=["download"])
 app.include_router(account.router, prefix="/account", tags=["account"])
@@ -14,6 +23,6 @@ app.add_event_handler("startup", startup)
 app.add_event_handler("shutdown", shutdown)
 
 
-@app.get("")
+@app.get("/")
 def read_root():
-    return {"message": "Welcome to CoursesDownloader Backend"}
+    return {"message": "Bem vindo ao Courses Downloader App"}
