0c0fd918ac24eb8ea6b38d64b503bcd2c3469c4c
Felipe Adeildo <felipe.adeildo0@gmail.com>
2024-05-30 01:13:52 -0300

    refactor(back): Move main to a higher level

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from app.prisma import shutdown, startup
+from app.routers import account, download, log, setting
+from fastapi import FastAPI
+
+app = FastAPI()
+
+app.include_router(download.router, prefix="/download", tags=["download"])
+app.include_router(account.router, prefix="/account", tags=["account"])
+app.include_router(log.router, prefix="/log", tags=["log"])
+app.include_router(setting.router, prefix="/setting", tags=["setting"])
+
+app.add_event_handler("startup", startup)
+app.add_event_handler("shutdown", shutdown)
+
+
+@app.get("")
+def read_root():
+    return {"message": "Welcome to CoursesDownloader Backend"}
