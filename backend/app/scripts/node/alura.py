0e1a4d0d82009d424d1ad21be4651b341a6eacf1
Felipe Adeildo <felipe.adeildo0@gmail.com>
2024-06-02 15:47:48 -0300

    feat(script): Add a new node level: `content` (lesson files)

--- backend/app/scripts/node/alura.py
+++ backend/app/scripts/node/alura.py
@@ -1,18 +1,18 @@
+import json
+
 from app.scripts.base.node import Node
-from app.scripts.base.utils import get_soup, valid_tag
+from app.scripts.base.utils import get_best_quality, get_soup, valid_tag
 
 
 class AluraNode(Node):
     BASE_URL = "https://cursos.alura.com.br"
 
-    async def download(self):
-        await self.load_children()
-
+    async def _download(self):
         for child in self.children:
             await child.download()
 
-        if self.type == "lesson":
-            await self.__download_lesson()
+        if self.type == "content":
+            await self.__download_content()
         else:
             await self.log("node_success_downloaded")
             await self.node_service.set_status(self.id, "downloaded")
@@ -21,6 +21,7 @@ class AluraNode(Node):
         loaders = {
             "course": self.__load_modules,
             "module": self.__load_lessons,
+            "lesson": self.__load_contents,
         }
 
         loader = loaders.get(self.type)
@@ -28,7 +29,6 @@ class AluraNode(Node):
             return
 
         await self.log("node_start_mapping_children")
-        await self.node_service.set_status(self.id, "mapping")
         await loader()
 
     async def __load_modules(self):
@@ -84,6 +84,7 @@ class AluraNode(Node):
             lesson = {
                 "name": name,
                 "type": "lesson",
+                "order": i,
                 "url": f"{self.BASE_URL}{lesson_tag.a.get('href')}",
                 **self.NODE_DEFAULTS,
             }
@@ -96,9 +97,47 @@ class AluraNode(Node):
             "node_success_mapped", count=len(self.children), children_type="lesson"
         )
 
-    async def __download_lesson(self):
+    async def __load_contents(self):
+        soup = await get_soup(self.session.get, self.url)
+
+        video_tag = valid_tag(soup.find("section", {"id": "video"}))
+        if video_tag:
+            await self.__get_video_content()
+
+        await self.node_service.set_status(self.id, "mapped")
+        await self.log(
+            "node_success_mapped", children_type="content", count=len(self.children)
+        )
+
+    async def __get_video_content(self):
+        video_qualities = (await self.session.get(f"{self.url}/video")).json()
+        best_quality = get_best_quality(v["quality"] for v in video_qualities)
+        for video in video_qualities:
+            video_dump = {
+                "name": f"Aula [{video['quality']}].mp4",
+                "type": "content",
+                "url": video["mp4"],
+                "extra_infos": json.dumps(video),
+                "should_download": video["quality"] == best_quality,
+                **self.NODE_DEFAULTS,
+            }
+
+            node = AluraNode(**video_dump)
+            await node.flush_node_db()
+            self.children.append(node)
+
+    async def __download_content(self):
+        if self.path.exists():
+            # TODO: check content size (bytes)
+            await self.log("content_already_downloaded")
+            return
+
         await self.log("node_start_download")
         await self.node_service.set_status(self.id, "downloading")
+        with self.path.open("wb") as f:
+            response = await self.session.get(self.url, stream=True)
+            for chunk in response.iter_content(1024):
+                f.write(chunk)
 
         await self.node_service.set_status(self.id, "downloaded")
         await self.log("node_success_downloaded")
